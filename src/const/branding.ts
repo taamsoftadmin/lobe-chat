357a171281be3736afcc1b3d07977c4e9bad81d5
TAMEEM HEZAM <admin@taamsoft.com>
2025-07-29 23:56:28 -0600

    🔧 feat: Update feature flags and branding information to reflect new commercial identity as TaamAI

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'Taam AI';
 
-export const BRANDING_NAME = 'LobeChat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_NAME = 'TaamAI';
+export const BRANDING_LOGO_URL = 'https://taam.ai/assets/logo1-BNSJsh1W.svg';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'TaamAI';
 
 export const BRANDING_URL = {
   help: undefined,
